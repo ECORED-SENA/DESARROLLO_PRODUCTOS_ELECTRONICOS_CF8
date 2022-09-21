7aba2cd6014f34aab99632ad380b2f40644bed04
Jhon Edinson Castañeda Oviedo <48686871+JhonEdinson-Shongo@users.noreply.github.com>
2022-09-20 22:09:13 -0500

    finalización maquetación

--- src/config/global.js
+++ src/config/global.js
@@ -34,14 +34,38 @@ export default {
         nombreRuta: 'tema1',
         icono: 'far fa-file-alt',
         numero: '1',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Tipos de aparatos eléctricos y electrónicos',
         desarrolloContenidos: true,
         subMenu: [
           {
             icono: 'far fa-file-alt',
             numero: '1.1',
-            titulo: 'Titulo de segundo nivel',
+            titulo: 'Residuo',
             hash: 't_1_1',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '1.2',
+            titulo: 'Equipamiento',
+            hash: 't_1_2',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '1.3',
+            titulo: 'Ética',
+            hash: 't_1_3',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '1.4',
+            titulo: 'Elementos de marca',
+            hash: 't_1_4',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '1.5',
+            titulo: 'Corrientes de materiales recuperados',
+            hash: 't_1_5',
           },
         ],
       },
@@ -50,24 +74,60 @@ export default {
         nombreRuta: 'tema2',
         icono: 'far fa-file-alt',
         numero: '2',
-        titulo: 'Titulo de primer nivel',
+        titulo:
+          'Clasificación de residuos de aparatos eléctricos y electrónicos',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            icono: 'far fa-file-alt',
+            numero: '2.1',
+            titulo:
+              'Separación y clasificación de residuos según sus características',
+            hash: 't_2_1',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '2.2',
+            titulo: 'Disposición final de residuos peligrosos',
+            hash: 't_2_2',
+          },
+        ],
       },
       {
         nombreRuta: 'tema3',
         icono: 'far fa-file-alt',
         numero: '3',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Normatividad ambiental',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            icono: 'far fa-file-alt',
+            numero: '3.1',
+            titulo: 'Riesgo',
+            hash: 't_3_1',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '3.2',
+            titulo: 'Almacenamiento de residuos',
+            hash: 't_3_2',
+          },
+          {
+            icono: 'far fa-file-alt',
+            numero: '3.3',
+            titulo: 'Técnicas de manipulación de cargas',
+            hash: 't_3_3',
+          },
+        ],
       },
     ],
     subMenu: [
-      {
-        icono: 'fas fa-sitemap',
-        titulo: 'Síntesis',
-        nombreRuta: 'sintesis',
-        desarrolloContenidos: true,
-      },
+      // {
+      //   icono: 'fas fa-sitemap',
+      //   titulo: 'Síntesis',
+      //   nombreRuta: 'sintesis',
+      //   desarrolloContenidos: true,
+      // },
       {
         nombreRuta: 'actividad',
         icono: 'far fa-question-circle',
@@ -103,32 +163,131 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
+      tema: 'Clasificación de residuos de aparatos eléctricos y electrónicos',
+      referencia:
+        'Ministerio del ambiente. (2010). <em>Lineamientos Técnicos para el Manejo de Residuos de Aparatos Eléctricos y Electrónicos.</em>',
+      tipo: 'Texto',
+      link:
+        'http://www.residuoselectronicos.net/wp-content/uploads/2012/03/Guia_RAEE_MADS_2011-reducida.pdf',
     },
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema: 'Clasificación de residuos de aparatos eléctricos y electrónicos',
+      referencia:
+        'Ministerio del medio ambiente. (s.f.) <em>Política nacional. Gestión integral de residuos de aparatos eléctricos y electrónicos.</em>',
+      tipo: 'Página <em>web</em>',
+      link:
+        'https://www.minambiente.gov.co/images/AsuntosambientalesySectorialyUrbana/pdf/e-book_rae_/contenido_2_1_1.html',
+    },
+    {
+      tema: 'Clasificación de residuos de aparatos eléctricos y electrónicos',
+      referencia:
+        'Camacho, E. (2016). <em>Consideraciones generales ara el desensamble.</em> SRI.',
+      tipo: 'Presentación',
+      descarga: '/downloads/Consideraciones_desensamble_RAEE_Camacho.pdf',
+    },
+    {
+      tema: 'Clasificación de residuos de aparatos eléctricos y electrónicos',
+      referencia:
+        'Hernández, C. (2016). <em>Gestión RAEE en Colombia.</em> SRI.',
+      tipo: 'Presentación',
+      descarga: '/downloads/Gestión_RAEE_Colombia_Hernandez.pdf',
+    },
+    {
+      tema: 'Clasificación de residuos de aparatos eléctricos y electrónicos',
+      referencia:
+        'Spitzbart, M. (2016). <em>Introducción al desensamble Manual de RAEE.</em> SRI.',
+      tipo: 'Presentación',
+      descarga: '/downloads/Introduccion_desensamble_Spitzbart.pdf',
+    },
+    {
+      tema: 'Clasificación de residuos de aparatos eléctricos y electrónicos',
+      referencia:
+        'ARL Positiva. (2015). <em>Guía de buenas prácticas control de peligros en la manipulación de cargas.</em>',
+      tipo: 'Texto',
+      link:
+        'https://posipedia.com.co/wp-content/uploads/2018/09/guia-buenas-practicas-control-peligro-manipulacion-cargas.pdf',
+    },
+    {
+      tema: 'Normatividad ambiental',
+      referencia:
+        'Unidad de planeación minero energética. (2017). <em>Normatividad Ambiental y Sanitaria.</em> UPME.',
+      tipo: 'Norma',
+      link:
+        'http://www.upme.gov.co/guia_ambiental/carbon/gestion/politica/normativ/normativ.htm#BM1__NORMATIVIDAD_GENERAL',
+    },
+    {
+      tema: 'Riesgo',
+      referencia:
+        'RT en Español. (2015). <em>El vertedero electrónico - Documental de RT</em> (Video). YouTube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=lGdgtUDE9TY',
     },
   ],
   glosario: [
     {
-      termino: '',
-      significado: '',
+      termino: 'Acumulador',
+      significado:
+        'Pila reversible que acumula energía durante la carga y la restituye en la descarga.',
     },
     {
-      termino: '<em></em>',
-      significado: '',
+      termino: 'Bromado',
+      significado: 'm. Quím. Sal del bromo con un metal.',
+    },
+    {
+      termino: 'Cátodo',
+      significado: 'm. Fís. Electrodo negativo.',
+    },
+    {
+      termino: 'Chip',
+      significado:
+        'm. Pequeña pieza de material semiconductor que contiene múltiples circuitos integrados con los que se realizan numerosas funciones en computadoras y dispositivos electrónicos.',
+    },
+    {
+      termino: 'Torx',
+      significado: 'Tipo de destornillador en forma de estrella.',
+    },
+    {
+      termino: 'Vertedero',
+      significado:
+        'm. Conducto por el que se arrojan a un depósito situado a nivel inferior basuras, desechos, ropa sucia, etc.',
     },
   ],
   referencias: [
     {
-      referencia: '',
+      referencia: 'ACS RECYCLING (2020). <em>Clasificación de los RAEE.</em>',
+      link:
+        'https://acsrecycling.es/wp-content/uploads/2020/06/clasificacion-de-los-raee.jpg',
+    },
+    {
+      referencia:
+        'ARL Positiva. (2015). <em>Guía de buenas prácticas y control de peligros en la manipulación de cargas.</em>',
+      link:
+        'https://posipedia.com.co/wp-content/uploads/2018/09/guia-buenas-practicas-control-peligro-manipulacion-cargas.pdf',
+    },
+    {
+      referencia:
+        'Camacho, E. (2016). Consideraciones generales ara el desensamble. SRI.',
       link: '',
+    },
+    {
+      referencia: 'PNUMA (s.f.). <em>Convenio de Basilea.</em> [pdf].',
+      link:
+        'https://www.basel.int/Portals/4/Basel%20Convention/docs/text/BaselConventionText-s.pdf',
+    },
+    {
+      referencia:
+        'Real Academia Española. (2020). <em>Diccionario de la lengua española.</em> RAE',
+      link: '',
+    },
+    {
+      referencia:
+        'Spitzbart, M. (2016). <em>Introducción al desensamble Manual de RAEE.</em> SRI.',
+      link: '',
+    },
+    {
+      referencia: 'UPME. (s.f.). Normatividad Ambiental General.',
+      link:
+        'http://www.upme.gov.co/guia_ambiental/carbon/gestion/politica/normativ/normativ.htm#BM1__NORMATIVIDAD_GENERAL',
     },
   ],
   creditos: {
--- src/router/index.js
+++ src/router/index.js
@@ -76,12 +76,12 @@ const router = new VueRouter({
           /* webpackChunkName: "referencias" */ '../../node_modules/ecored-base-pkg/src/views/Referencias.vue'
         ),
     },
-    {
-      path: '/sintesis',
-      name: 'sintesis',
-      component: () =>
-        import(/* webpackChunkName: "sintesis" */ '../views/sintesis.vue'),
-    },
+    // {
+    //   path: '/sintesis',
+    //   name: 'sintesis',
+    //   component: () =>
+    //     import(/* webpackChunkName: "sintesis" */ '../views/sintesis.vue'),
+    // },
     {
       path: '/creditos',
       name: 'creditos',
